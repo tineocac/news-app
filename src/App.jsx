46988a23b410482a257285048dd444e5f083d200
tineocac <carlostineocac@gmail.com>
2022-12-07 14:14:52 -0400

    feat: navbar was created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import './assets/styles/App.css'
+import MyNavBar from './components/MyNavBar'
 import { Favorites, Home, Login, NewDetails } from './pages'
 
 function App() {
 
   return (
     <HashRouter>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/favorites' element={<Favorites />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/news/:id' element={<NewDetails />} />
-        </Routes>
-      </div>
+      <MyNavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favorites' element={<Favorites />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/news/:id' element={<NewDetails />} />
+      </Routes>
     </HashRouter>
   )
 }
---
+++ src/components/MyNavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+
+const MyNavBar = () => {
+    return (
+        <Navbar bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to={'/'}>NEWS APP</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to={'/favorites'}>Favorites</Nav.Link>
+                    <Nav.Link as={Link} to={'/login'}>Login</Nav.Link>
+                    <Nav.Link >(sidebar)</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default MyNavBar;
